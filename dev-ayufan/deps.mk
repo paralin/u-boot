a470662bfaaec036be0c22a85ff0b45211a25b20
Kamil Trzciński <ayufan@ayufan.eu>
2020-04-03 15:47:59 +0200

    ayufan: dev.mk: allow to compile ATF on-demand

---
+++ dev-ayufan/deps.mk
@@ -0,0 +1,11 @@
+$(filter tmp/rkbin/%, $(BL31) $(UBOOT_TPL) $(UBOOT_SPL) $(LOADER_BIN)):
+	mkdir -p $$(dirname "$@")
+	curl --fail -L https://github.com/ayufan-rock64/rkbin/raw/master/$(subst tmp/rkbin/,,$@) > $@.tmp
+	mv $@.tmp $@
+
+$(filter tmp/atf/%, $(BL31)):
+	rm -rf $$(dirname "$@")/$$(basename "$@" .elf)/src
+	git clone https://github.com/ARM-software/arm-trusted-firmware.git $$(dirname "$@")/$$(basename "$@" .elf)/src
+	git -C $$(dirname "$@")/$$(basename "$@" .elf)/src checkout $$(basename "$@" .elf)
+	make -C $$(dirname "$@")/$$(basename "$@" .elf)/src PLAT=$(BOARD_CHIP) CROSS_COMPILE="ccache aarch64-linux-gnu-" M0_CROSS_COMPILE="ccache arm-linux-gnueabi-" bl31
+	cp $$(dirname "$@")/$$(basename "$@" .elf)/src/build/rk3399/release/bl31/bl31.elf $@
