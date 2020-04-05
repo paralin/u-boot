13cb97eaf5d30d572e01a589d85f8514c7277070
Kamil Trzciński <ayufan@ayufan.eu>
2020-04-05 12:17:22 +0200

    ayufan: rockpro64: support SPI boot

--- arch/arm/mach-rockchip/rk3399/rk3399.c
+++ arch/arm/mach-rockchip/rk3399/rk3399.c
@@ -23,7 +23,7 @@ DECLARE_GLOBAL_DATA_PTR;
 
 const char * const boot_devices[BROM_LAST_BOOTSOURCE + 1] = {
 	[BROM_BOOTSOURCE_EMMC] = "/sdhci@fe330000",
-	[BROM_BOOTSOURCE_SPINOR] = "/spi@ff1d0000",
+	[BROM_BOOTSOURCE_SPINOR] = "/spi@ff1d0000/flash@0",
 	[BROM_BOOTSOURCE_SD] = "/dwmmc@fe320000",
 };
 
@@ -174,7 +174,7 @@ const char *spl_decode_boot_device(u32 boot_device)
 	} spl_boot_devices_tbl[] = {
 		{ BOOT_DEVICE_MMC1, "/dwmmc@fe320000" },
 		{ BOOT_DEVICE_MMC2, "/sdhci@fe330000" },
-		{ BOOT_DEVICE_SPI, "/spi@ff1d0000" },
+		{ BOOT_DEVICE_SPI, "/spi@ff1d0000/flash@0" },
 	};
 
 	for (i = 0; i < ARRAY_SIZE(spl_boot_devices_tbl); ++i)
