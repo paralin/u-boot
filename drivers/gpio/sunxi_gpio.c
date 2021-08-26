9d29038eeb51a36fe5f1ae05cd962981ea679e98
Samuel Holland <samuel@sholland.org>
2021-08-26 18:02:54 -0500

    gpio: axp/sunxi: Remove virtual VBUS enable GPIO
    Now that this functionality is modeled using the device tree and
    regulator uclass, the named GPIO is not referenced anywhere. Remove
    it, along with the rest of the support for AXP virtual GPIOs.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- drivers/gpio/sunxi_gpio.c
+++ drivers/gpio/sunxi_gpio.c
@@ -114,15 +114,7 @@ int sunxi_name_to_gpio(const char *name)
 {
 	unsigned int gpio;
 	int ret;
-#if !defined CONFIG_SPL_BUILD && defined CONFIG_AXP_GPIO
-	char lookup[8];
 
-	if (strcasecmp(name, "AXP0-VBUS-ENABLE") == 0) {
-		sprintf(lookup, SUNXI_GPIO_AXP0_PREFIX "%d",
-			SUNXI_GPIO_AXP0_VBUS_ENABLE);
-		name = lookup;
-	}
-#endif
 	ret = gpio_lookup_name(name, NULL, NULL, &gpio);
 
 	return ret ? ret : gpio;
